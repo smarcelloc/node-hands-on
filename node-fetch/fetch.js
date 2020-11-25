40040a70fd1d5f619de555b0b6030365ec0cc1e5
Marcello Silvério <smarcelloc@gmail.com>
2020-11-25 23:39:26 +0000

    node-fetch

---
+++ node-fetch/fetch.js
@@ -0,0 +1,22 @@
+const fetch = require('node-fetch')
+
+// API EXTERNA
+const apiUrl = 'https://jsonplaceholder.typicode.com/todos'
+const apiUrlPost = 'https://httpbin.org/post'
+fetch(apiUrl)
+  .then(res => res.json())
+  .then(json => console.log(json))
+  .catch(err => { console.log(err); process.exit(1); });
+
+fetch(apiUrlPost, {
+  method: 'POST',
+  body: {
+    name: 'julio',
+    age: 2
+  }
+})
+  .then(json => console.log(json))
+  .catch(err => { console.log(err); process.exit(1); });
+
+
+// Atenção: node-fetch vs windows.fetch (browser)
