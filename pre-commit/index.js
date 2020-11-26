332154e7850809716b9cb933621b6735566dd079
Marcello Silvério <smarcelloc@gmail.com>
2020-11-26 01:58:56 +0000

    Pre-Commit & Jest

---
+++ pre-commit/index.js
@@ -0,0 +1,3 @@
+exports.dobro = (num) => {
+  return num * 2;
+}
---
+++ pre-commit/test/index.test.js
@@ -0,0 +1,12 @@
+const index = require('./../index')
+
+test('Verificar se a função dobro do index está funcionando', () => {
+  const num = 3;
+  const resultado = index.dobro(3);
+
+  expect(resultado).toBe(num * 2);
+});
+
+
+
+
